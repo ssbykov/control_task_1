1a5b88f9914abc33cef5e75b8b174a78eeecb934
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-03 12:55:55 +0300

    убрал return

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,32 +20,24 @@ fun main() {
     println(agoToText(72 * 60 * 60 + 1))
 }
 
-fun agoToText(seconds: Int): String {
-    val res = when (seconds) {
+fun agoToText(seconds: Int) = when (seconds) {
         in (0..60) -> "только что"
         in (61..60 * 60) -> "${agoToMinutes(seconds / 60)} назад"
         in (60 * 60 + 1..24 * 60 * 60) -> "${agoToHours(seconds / (60 * 60))} назад"
         in (24 * 60 * 60 + 1..48 * 60 * 60) -> "вчера"
         in (48 * 60 * 60 + 1..72 * 60 * 60) -> "позавчера"
         else -> "давно"
-    }
-    return res
 }
 
-fun agoToMinutes(minutes: Int): String {
-    val result = when {
-        minutes % 10 == 1 && minutes != 11 -> "$minutes минуту"
-        minutes % 10 in (2..4) && minutes !in (12..14) -> "$minutes минуты"
-        else -> "$minutes минут"
-    }
-    return result
+fun agoToMinutes(minutes: Int) = when {
+    minutes % 10 == 1 && minutes != 11 -> "$minutes минуту"
+    minutes % 10 in (2..4) && minutes !in (12..14) -> "$minutes минуты"
+    else -> "$minutes минут"
 }
 
-fun agoToHours(hours: Int): String {
-    val result = when {
-        hours % 10 == 1 && hours != 11 -> "$hours час"
-        hours % 10 in (2..4) && hours !in (12..14) -> "$hours часа"
-        else -> "$hours часов"
-    }
-    return result
+
+fun agoToHours(hours: Int) = when {
+    hours % 10 == 1 && hours != 11 -> "$hours час"
+    hours % 10 in (2..4) && hours !in (12..14) -> "$hours часа"
+    else -> "$hours часов"
 }
